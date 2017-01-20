4141a84d563121e872e6cc94940b8f912980eb4e
victor foreman <v@victors-MacBook-Pro.local>
2017-01-20 13:08:25 -0500

    handle the empty list

--- fp.rkt
+++ fp.rkt
@@ -23,9 +23,16 @@
   )
   
 )
-
+;Write a function (sum-up-numbers-simple L). L is a list, which may contain
+;as elements numbers and non-numbers. The result of the function is the sum of the numbers
+;not in nested lists in L. If there are no such numbers, the result is zero. For example, the result
+;of (sum-up-numbers-simple ‘(a b 1 2 c 3 d)) should be 6.
 (define (sum-up-numbers-simple L)
-  1
+  ;handle the empty list
+  (if (null? L)
+      0 ;if null return 0
+      ()
+  )
 )
 
 
