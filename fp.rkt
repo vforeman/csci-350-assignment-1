46b3daead34e3e4df57999989a960ea34fb52ef7
victor foreman <v@victors-MacBook-Pro.local>
2017-01-19 22:04:51 -0500

    clean comments and indenting

--- fp.rkt
+++ fp.rkt
@@ -1,15 +1,23 @@
-(define testlist '((1 2 3) (4 5 6))); test list
+(define testlist '(1 2 3 (1 2 3) (4 5 6))); test list
 
 (define (reverse-general L)
   (if
-    (null? L); if null then return nil
-    '()
-    (if (list? (car L))
-        ;(append (reverse-general (cdr L)) (reverse-general (car L)) ); return the cons of the cdr and car
-
-        (reverse-general (car L) ); return the cons of the cdr and car
-        (append (reverse-general (cdr L)) (list (car L)) ); return the cons of the cdr and car
-
+    (null? L) 
+    '() ; if null, return an empty list
+    (if (list? (car L)) ;check if the head element in the list is also a list
+        (append ;if the popped element is also a list, then we are dealing with 2 lists that each need to be reversed
+         (reverse-general (cdr L)) ;reverse the remaining list
+         (list
+          (reverse-general (car L));also reverse the popped list
+         )
+        ); return the cons of the cdr and car
+        ;(reverse-general (car L) ); return the cons of the cdr and car
+        (append;if the popped element is not a list, then only the remaining list need to reveresd, the popped element is an atom
+         (reverse-general (cdr L));reverse the remaining list
+         (list
+          (car L);no need to reverse a single atom
+         )
+        ); return the cons of the cdr and car
     )
   )
   
