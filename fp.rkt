a81d934629c7702549cb4809b587aaefff0b38c5
victor foreman <v@victors-MacBook-Pro.local>
2017-01-24 22:14:40 -0500

    cleaning up the code

--- fp.rkt
+++ fp.rkt
@@ -1,7 +1,4 @@
-(define testlist '(1 2 3 (1 2 3) (4 5 6))); test list
-(define testlist2 '((1 2 3) 100 200 (1 2 3)))
-(define testlist3 '(a 300 b 200 c 200 d))
-(define testlist4 '(a 200 b 300 c 100 d))
+
 
 (define (reverse-general L)
   (if
@@ -25,15 +22,12 @@
   )
   
 )
-;Write a function (sum-up-numbers-simple L). L is a list, which may contain
-;as elements numbers and non-numbers. The result of the function is the sum of the numbers
-;not in nested lists in L. If there are no such numbers, the result is zero. For example, the result
-;of (sum-up-numbers-simple ‘(a b 1 2 c 3 d)) should be 6.
+
 (define (sum-up-numbers-simple L)
   ;handle the empty list
   (if (null? L)
       0 ;if not given a list then return 0
-      (if (list? (car L))
+      (if (or (list? (car L)) (not (number? (car L)))); if the element is a list or nan
           (+ 0 (sum-up-numbers-simple (cdr L)));if the element is a list then add 0 to the sum of the remaining list
           (+ (car L) (sum-up-numbers-simple (cdr L)));else add that element to the recursive sum of the remaining list
       )
@@ -44,9 +38,14 @@
   ;handle the empty list
   (if (null? L)
       0 ;if not given a list then return 0
-      (if (list? (car L))
-          (+ (sum-up-numbers-general (car L)) (sum-up-numbers-simple (cdr L)));if the element is a list then add general sum of that list to the sum of the remaining list
-          (+ (car L) (sum-up-numbers-simple (cdr L)));else add that element to the recursive sum of the remaining list
+      (if (list? (car L)); if the element is a list 
+          (+ (sum-up-numbers-general (car L)) (sum-up-numbers-general(cdr L)));if the element is a list then add general sum of that list to the sum of the remaining list
+
+          (if (not (number? (car L))); if elem 1 is nan
+              (+ 0 (sum-up-numbers-general (cdr L)));then add 0 to the sum
+              (+ (car L) (sum-up-numbers-general(cdr L)));else add that element to the recursive sum of the remaining list
+          )
+          
       )
   )
 )  
@@ -55,15 +54,9 @@
  (cond 
    (
     (and (not (null? L1)) (not (null? L2)));neither list null
-    (display "neither list is null")(newline)
-    (display L1)(newline)
-        (display L2)(newline)
-
     (cond ;compare elements
      (
       (and (number? (car L1)) (number? (smallest-num-from-list-simple L2))); case: num-num
-          (display "num-num")(newline)
-
       (if (> (car L1) (smallest-num-from-list-simple L2)); if num1 > num2
           (if (and (not (null? (cdr L1))) (number? (car (cdr L1))))
               (if (<= (car L1) (car (cdr L1))); if l1[0] < l1[1]
@@ -73,37 +66,28 @@
               (min-above-min L1 '());else this is the last elemen in l1 so recall with l2 null
           )
          (min-above-min (cdr L1) (list (smallest-num-from-list-simple L2)));else pop elem1 and recall
-
        )
       )
      (
       (and (number? (car L1)) (not (number? (smallest-num-from-list-simple L2)))); case: num-nan
-          (display "num-nan")(newline)
-
       (min-above-min L1 '());then recall with l2 null
       )
      (
       (and (not (number? (car L1))) (number? (smallest-num-from-list-simple L2))); case: nan-num
-          (display "nan-num")(newline)
-
       (min-above-min (cdr L1) (list (smallest-num-from-list-simple L2)));then pop elem1 and recall
       )
      (
       (and (not (number? (car L1))) (not (number? (smallest-num-from-list-simple L2)))); case: nan-nan
-          (display "nan-nan")(newline)
-
       (min-above-min (cdr L1) '());then pop both elems and recall
      )
     )
    )
    (
     (and (null? L1) (not (null? L2)));l1 null
-    (display "L1 is null")(newline)
     #f
    )
    (
     (and (not (null? L1)) (null? L2));l2 null
-    (display "L2 is null")(newline)
     (smallest-num-from-list-simple L1);return the smallest elem from l1
    )
  )
@@ -124,22 +108,22 @@
       )
   )
 )
-
-(define (atom? x) 
-  (and (not (pair? x))
-       (not (null? x))))
-
-
-(define (test)
-  ;(display "test one")(newline)
-  ;(reverse-general testlist)
-  ;(display "test two")(newline)
-  ;(sum-up-numbers-simple testlist2)
-  ;(display "test two")(newline)
-  ;(sum-up-numbers-general testlist2)
-  (display "test three")(newline)
-  (min-above-min testlist3 testlist4)
-  ;(display "test four")(newline)
-  ;(smallest-num-from-list-simple testlist3)
-
+(define (largest-num-from-list-simple L);given a list, return the largest num
+  (if (null? L)
+      #F
+      (if (not(number? (car L))) ;if the first elem is not a num
+          (largest-num-from-list-simple (cdr L)); pop it off and recall
+          (if (null? (cdr L));else if theres only one element in the list and it is a number now
+              (car L);then return that number
+              (if (or (not (number? (car (cdr L)))) (> (car L) (car (cdr L)) ));if the 2nd elem is not a number or if the 1st elem is larger than the 2nd elem
+                  (largest-num-from-list-simple (append (cdr (cdr L)) (list (car L))));then append elem1 to the end and pop off the 2nd elem before recalling
+                  (largest-num-from-list-simple (cdr L));else pop the 1st element
+              )
+          )
+      )
+  )
 )
+
+
+
+
